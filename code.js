cbb001345f967827e17f748c0da183c4d4730195
AidenEyre <EyreAiden@gmail.com>
2020-10-09 13:44:09 -0600

    Adjusted more random css styles, nothing important.

--- code.js
+++ code.js
@@ -2,16 +2,16 @@
   JS code to handle the fieldset transitions with html buttons.
    
    Author: Aiden Eyre
-   Date:   27 September 2020
+   Date:   08 Octo 2020
    
    Filename: code.js
 */
 
-this.clicked = "aboutMeSection"; // Placeholder of current fieldset.
+this.clicked = "aboutMeSection"; // Placeholder of current div.
 
 // I add window.onload so that the DOM can fully load before executing.
 window.onload = function () {
-  // Set up event listener for each fieldset button.
+  // Set up event listener for each div button.
   var aboutMeButton = document
     .getElementById("aboutMeButton")
     .addEventListener("click", (evt) => {
@@ -36,7 +36,7 @@ window.onload = function () {
       transitions("contactMeSection");
     });
 
-  // Function to change css styles to show a feildset transition.
+  // Function to change css styles to show a div transition.
   var transitions = function (clickedButton) {
     document.getElementById(this.clicked).style.display = "none";
     document.getElementById(clickedButton).style.display = "flex";
